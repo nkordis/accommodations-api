00de27790c9f24dc6e397c16227757c8ac1aa930
nkordis <nikoskordis@gmail.com>
2024-06-10 17:55:07 +0200

    feat: Add EF Core DbContext for Accommodations

---
+++ Accommodations.Infra/Persistence/AccommodationsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Accommodations.Domain.Entities;
+
+namespace Accommodations.Infra.Persistence
+{
+    internal class AccommodationsDbContext : DbContext
+    {
+        internal DbSet<Accommodation> Accommodations { get; set; }
+        internal DbSet<Unit> Units { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AccommodationsDb;Trusted_Connection=True;");
+        }
+    }
+}
