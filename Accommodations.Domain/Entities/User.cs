e1da7dafd393779980cc7e6a113085fcb787d613
nkordis <nikoskordis@gmail.com>
2024-06-22 13:23:14 +0200

    feat: Extend identity user with custom properties and update database schema

--- Accommodations.Domain/Entities/User.cs
+++ Accommodations.Domain/Entities/User.cs
@@ -4,5 +4,7 @@ namespace Accommodations.Domain.Entities
 {
     public class User : IdentityUser
     {
+        public DateOnly? DateOfBirth { get; set; }
+        public string? Nationality { get; set; }
     }
 }
--- Accommodations.Infra/Migrations/20240622101802_UserAdditionalPropertiesAddded.Designer.cs
+++ Accommodations.Infra/Migrations/20240622101802_UserAdditionalPropertiesAddded.Designer.cs
@@ -4,6 +4,7 @@ using Accommodations.Infra.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Accommodations.Infra.Migrations
 {
     [DbContext(typeof(AccommodationsDbContext))]
-    partial class AccommodationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240622101802_UserAdditionalPropertiesAddded")]
+    partial class UserAdditionalPropertiesAddded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
