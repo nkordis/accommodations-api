e27544cc34de58cbc518dbdac337b90d0ad95453
nkordis <nikoskordis@gmail.com>
2024-06-30 10:38:21 +0200

    test: Add unit tests for CurrentUser.IsInRole method

---
+++ Accommodations.AppTests/User/CurrentUserTests.cs
@@ -0,0 +1,53 @@
+﻿using Accommodations.Domain.Constants;
+using FluentAssertions;
+using Xunit;
+
+namespace Accommodations.App.User.Tests
+{
+    public class CurrentUserTests
+    {
+        [Theory()]
+        [InlineData(UserRoles.Admin)]
+        [InlineData(UserRoles.User)]
+        public void IsInRole_WithMatchingRole_ShouldReturnTrue(string roleName)
+        {
+            // Arrange
+            var currentUser = new CurrentUser("21B6BD26-B363-43BA-BCA4-898367AB452E", "test@test.com",
+                [UserRoles.Admin, UserRoles.User], null, null);
+
+            // Act
+            var isInRole = currentUser.IsInRole(roleName);
+
+            // Assert
+            isInRole.Should().BeTrue();
+        }
+
+        [Fact()]
+        public void IsInRole_WithNoMatchingRole_ShouldReturnFalse()
+        {
+            // Arrange
+            var currentUser = new CurrentUser("21B6BD26-B363-43BA-BCA4-898367AB452E", "test@test.com",
+                [UserRoles.Admin, UserRoles.User], null, null);
+
+            // Act
+            var isInRole = currentUser.IsInRole(UserRoles.Owner);
+
+            // Assert
+            isInRole.Should().BeFalse();
+        }
+
+        [Fact()]
+        public void IsInRole_WithNoMatchingRoleCase_ShouldReturnFalse()
+        {
+            // Arrange
+            var currentUser = new CurrentUser("21B6BD26-B363-43BA-BCA4-898367AB452E", "test@test.com",
+                [UserRoles.Admin, UserRoles.User], null, null);
+
+            // Act
+            var isInRole = currentUser.IsInRole(UserRoles.Admin.ToLower());
+
+            // Assert
+            isInRole.Should().BeFalse();
+        }
+    }
+}
--- Accommodations.Application/User/CurrentUser.cs
+++ Accommodations.Application/User/CurrentUser.cs
@@ -2,7 +2,7 @@
 namespace Accommodations.App.User
 {
     public record CurrentUser(string Id, string Email, IEnumerable<string> Roles, 
-        string Nationality, DateOnly? DateOfBirth)
+        string? Nationality, DateOnly? DateOfBirth)
     {
         public bool IsInRole(string roleName) => Roles.Contains(roleName);
     }
