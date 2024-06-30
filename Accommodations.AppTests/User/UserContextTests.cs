b118949beb96ca1f85e2ad4833d14ea4a7ab404d
nkordis <nikoskordis@gmail.com>
2024-06-30 11:47:30 +0200

    test: Add unit tests for UserContext.GetCurrentUser method

---
+++ Accommodations.AppTests/User/UserContextTests.cs
@@ -0,0 +1,156 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
+using System.Security.Claims;
+using Accommodations.Domain.Constants;
+
+namespace Accommodations.App.User.Tests
+{
+    public class UserContextTests
+    {
+        [Fact()]
+        public void GetCurrentUser_WithAuthenticatedUser_ShouldReturnCurrentUser()
+        {
+            // Arrange
+            var dateOfBirth = new DateOnly(1990, 1, 1);
+
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            var claims = new List<Claim>()
+            {
+                new(ClaimTypes.NameIdentifier, "1"),
+                new(ClaimTypes.Email, "user@test.com"),
+                new(ClaimTypes.Role, UserRoles.Admin),
+                new(ClaimTypes.Role, UserRoles.User),
+                new("Nationality", "German"),
+                new("DateOfBirth", dateOfBirth.ToString("yyyy-MM-dd"))
+            };
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+            {
+                User = user
+            });
+
+            var userContext = new UserContext(httpContextAccessorMock.Object);
+
+            // Act
+            var currentUser = userContext.GetCurrentUser();
+
+            //Assert
+            currentUser.Should().NotBeNull();
+            currentUser.Id.Should().Be("1");
+            currentUser.Email.Should().Be("user@test.com");
+            currentUser.Roles.Should().ContainInOrder(UserRoles.Admin, UserRoles.User);
+            currentUser.Nationality.Should().Be("German");
+            currentUser.DateOfBirth.Should().Be(dateOfBirth);   
+        }
+
+        [Fact]
+        public void GetCurrentUser_WithUserContextNotPresent_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns((HttpContext)null);
+
+            var userContext = new UserContext(httpContextAccessorMock.Object);
+
+            // Act
+            Action action = () => userContext.GetCurrentUser();
+
+            // Assert
+            action.Should().Throw<InvalidOperationException>()
+                .WithMessage("User context is not present");
+        }
+
+        [Fact()]
+        public void GetCurrentUser_WithUnauthenticatedUser_ShouldReturnNull()
+        {
+            // Arrange
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            var claims = new List<Claim>()
+            {
+                new(ClaimTypes.NameIdentifier, "1"),
+                new(ClaimTypes.Email, "user@test.com"),
+                new("Nationality", "German"),
+                new("DateOfBirth", "1990-01-01")
+            };
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims));
+
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+            {
+                User = user
+            });
+
+            var userContext = new UserContext(httpContextAccessorMock.Object);
+
+            // Act
+            var currentUser = userContext.GetCurrentUser();
+
+            //Assert
+            currentUser.Should().BeNull();
+        }
+
+        [Fact()]
+        public void GetCurrentUser_WithInvalidDateFormat_ShouldThrowException()
+        {
+            // Arrange
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            var claims = new List<Claim>()
+            {
+                new(ClaimTypes.NameIdentifier, "1"),
+                new(ClaimTypes.Email, "user@test.com"),
+                new(ClaimTypes.Role, UserRoles.Admin),
+                new("Nationality", "German"),
+                new("DateOfBirth", "01-01-1990")
+            };
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+            {
+                User = user
+            });
+
+            var userContext = new UserContext(httpContextAccessorMock.Object);
+
+            // Act
+            Action action = () => userContext.GetCurrentUser();
+
+            // Assert
+            action.Should().Throw<FormatException>();
+        }
+
+        [Fact()]
+        public void GetCurrentUser_WithNoRoles_ShouldReturnUserWithoutRoles()
+        {
+            // Arrange
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            var claims = new List<Claim>()
+            {
+                new(ClaimTypes.NameIdentifier, "1"),
+                new(ClaimTypes.Email, "user@test.com"),
+                new("Nationality", "German"),
+                new("DateOfBirth", "1990-01-01")
+            };
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+            {
+                User = user
+            });
+
+            var userContext = new UserContext(httpContextAccessorMock.Object);
+
+            // Act
+            var currentUser = userContext.GetCurrentUser();
+
+            // Assert
+            currentUser.Should().NotBeNull();
+            currentUser.Roles.Should().BeEmpty();
+        }
+    }
+}
